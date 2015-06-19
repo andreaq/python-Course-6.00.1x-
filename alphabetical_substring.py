3d8becd3443d29f02b769d166af8acbb2e2d20b9
andreaq <q.andrea@hotmail.it>
2015-06-19 19:04:27 +0200

    Create alphabetical_substring.py

---
+++ alphabetical_substring.py
@@ -0,0 +1,21 @@
+# prints the longest substring of s in which the letters occur in alphabetical order
+
+s="azcbobobegghakl"
+
+a=[]
+string=""
+i=0
+check = list(s)
+for letter in list(s[0:len(s)-1]):
+    if check[i]<=check[i+1]:
+        string = string+check[i]
+    else:
+        a.append(string+check[i])
+        string=""
+    i+=1
+a.append(string+check[i])
+max_string = max(len(str(x)) for x in a)
+for sub in a:
+    if len(sub)==max_string:
+        print "Longest substring in alphabetical order is:{}".format(sub)
+        break
