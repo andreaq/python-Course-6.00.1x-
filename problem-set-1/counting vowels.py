75e4c0359c9a16a902d5f91a3d477d2ed690ef15
Andrea Quartarone <q.andrea@hotmail.it>
2015-07-11 17:22:03 +0200

    Create counting vowels.py

---
+++ problem-set-1/counting vowels.py
@@ -0,0 +1,16 @@
+'''
+Assume s is a string of lower case characters.
+
+Write a program that counts up the number of vowels contained in the string s. 
+Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
+'''
+
+
+vowels = list('aeiou')
+count = 0
+for letter in s:
+    if letter in vowels:
+        count+=1
+print 'Number of vowels: {}'.format(count)
